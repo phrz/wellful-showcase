f007c2f5a45e60a615ef4726c49787a6199a81a1
phrz <phrz@me.com>
2018-05-23 22:03:20 -0500

    straightened heart edges, changed js to wait for DOM

--- assets/wellful-showcase.js
+++ assets/wellful-showcase.js
@@ -5,14 +5,18 @@ function isOnScreen(e) {
 	return !(rect.bottom < 0 || rect.top - viewHeight >= 0)
 }
 
-const elements = document.querySelectorAll('[test-onscreen]')
+document.addEventListener("DOMContentLoaded", () => {
 
-window.onscroll = () => {
-	elements.forEach((e) => {
-		e.setAttribute('is-onscreen', isOnScreen(e))
-		e.setAttribute(
-			'once-onscreen', 
-			e.getAttribute('once-onscreen') == 'true' || isOnScreen(e)
-		)
-	})
-};
+	const elements = document.querySelectorAll('[test-onscreen]')
+
+	window.addEventListener('scroll', () => {
+		elements.forEach((e) => {
+			e.setAttribute('is-onscreen', isOnScreen(e))
+			e.setAttribute(
+				'once-onscreen', 
+				e.getAttribute('once-onscreen') == 'true' || isOnScreen(e)
+			)
+		})
+	}, false)
+
+})
